28d8c74419eccc9894d61b3ed4ea6a2711512a8d
Theodora <57870813+TheoRomanova@users.noreply.github.com>
2020-04-04 14:34:07 +0300

    Initial Commit

---
+++ DailyOracle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyOracle
+//
+//  Created by Theodora on 4/4/20.
+//  Copyright © 2020 Feodora Andryieuskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
